5730695b327a4a2566ba322e414596c816de4a13
tsh1no <rirakkuma.music.2nd@gmail.com>
2016-04-13 22:32:03 +0900

    symfony2.8.4のインストールとバンドルの作成

---
+++ dev_symfony/README.md
@@ -0,0 +1,4 @@
+dev_symfony
+===========
+
+A Symfony project created on April 13, 2016, 3:12 pm.
---
+++ dev_symfony/src/System/CoreBundle/SystemCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace System\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SystemCoreBundle extends Bundle
+{
+}
